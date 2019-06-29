d303560ff9614180d0dd2d111e94c3c07cd7a8ab
alasalle <angela.lasalle91@gmail.com>
2019-06-28 20:36:31 -0400

    ring_buffer complete

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,14 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    if self.current == self.capacity:
+      self.current = 0
+    self.storage[self.current] = item
+    self.current += 1
 
   def get(self):
-    pass
+    copy = self.storage[:]
+    for i, s in enumerate(self.storage):
+      if s == None:
+        copy.pop(i)
+    return copy
